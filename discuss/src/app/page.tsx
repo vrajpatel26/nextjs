fde8d58bf6af6dacd543e5bc36864ab22048709d
vrajpatel26 <vraj.26112004@gmail.com>
2026-08-06 12:09:49 +0530

    start new project - discuss app

---
+++ discuss/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div>Home</div>
+      <Button>Let's Build discuss App</Button>
+    </div>
+  )
+}
+
+export default page
